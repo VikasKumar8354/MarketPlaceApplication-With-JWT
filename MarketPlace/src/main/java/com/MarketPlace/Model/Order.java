e99ad2cf33cb412e29a6ee4bb1f7bca99af98b5b
VikasKumar8354 <120902vikaskumar@gmail.com>
2025-11-15 03:27:21 +0530

    updates

--- MarketPlace/src/main/java/com/MarketPlace/Model/Order.java
+++ MarketPlace/src/main/java/com/MarketPlace/Model/Order.java
@@ -35,6 +35,6 @@ public class Order {
     @ManyToOne(cascade = CascadeType.ALL)
     private Address shippingAddress;
 
-    @Embedded
+    @OneToOne(cascade = CascadeType.ALL)
     private PaymentInfo payment;
 }
