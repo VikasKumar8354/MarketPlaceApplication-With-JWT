95b62c1b31b4131a7b47ea871db00c63ece5ac35
VikasKumar8354 <120902vikaskumar@gmail.com>
2026-02-11 18:52:49 +0530

    Service updates

--- MarketPlace/src/main/java/com/MarketPlace/Repository/ProductRepository.java
+++ MarketPlace/src/main/java/com/MarketPlace/Repository/ProductRepository.java
@@ -1,11 +1,7 @@
 package com.MarketPlace.Repository;
 
 
-import com.MarketPlace.Model.Category;
 import com.MarketPlace.Model.Product;
-import com.MarketPlace.Model.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
